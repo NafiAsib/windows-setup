9ee7cab1652413f17a1902578a8518c396368543
NafiAsib <nafi.asib@gmail.com>
2022-02-18 22:33:39 +0600

    Added few package.

--- debloat.ps1
+++ debloat.ps1
@@ -1,6 +1,9 @@
 $choices = '&Yes', '&No'
 
 $xbox = $Host.UI.PromptForChoice('', 'Remove Xbox apps?', $choices, 1)
+$skype = $Host.UI.PromptForChoice('', 'Remove Skype?', $choices, 1)
+$office_one = $Host.UI.PromptForChoice('', 'Remove Office One apps?', $choices, 1)
+
 if ($xbox -eq 0) {
     Get-AppxPackage *Microsoft.Xbox.TCUI* | Remove-AppxPackage
     Get-AppxPackage *Microsoft.XboxApp* | Remove-AppxPackage
@@ -10,7 +13,12 @@ if ($xbox -eq 0) {
     Get-AppxPackage *Microsoft.XboxSpeechToTextOverlay* | Remove-AppxPackage
 }
 
+if ($skype -eq 0) {
+    Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage
+}
+
 Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.MSPaint* | Remove-AppxPackage
@@ -20,8 +28,6 @@ Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Office.OneNote* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.People* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.ScreenSketch* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.WindowsAlarms* | Remove-AppxPackage
@@ -32,4 +38,11 @@ Get-AppxPackage *Microsoft.WindowsSoundRecorder* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.YourPhone* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage
-C:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
+
+if ($office_one -eq 0) {
+    Get-AppxPackage *Microsoft.Office.OneNote* | Remove-AppxPackage
+    Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage
+    Get-AppxPackage *Microsoft.Office.Sway* | Remove-AppxPackage
+    Get-AppxPackage *Microsoft.Office.Todo.List* | Remove-AppxPackage
+    C:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
+}
